a7d62ff81d711bd68a2e81625b3adb2a2af26953
mohit-aren <26.mohit@gmail.com>
2024-03-30 18:57:47 +0530

    Update SegNet_breast_cancer.py
    Removed some unwanted code

--- SegNet_breast_cancer.py
+++ SegNet_breast_cancer.py
@@ -34,18 +34,6 @@ n_labels = 2
 
 Lung = [255,255,255]
 
-Sky = [128,128,128]
-Building = [128,0,0]
-Pole = [192,192,128]
-Road_marking = [255,69,0]
-Road = [128,64,128]
-Pavement = [60,40,222]
-Tree = [128,128,0]
-SignSymbol = [192,128,128]
-Fence = [64,64,128]
-Car = [64,0,128]
-Pedestrian = [64,64,0]
-Bicyclist = [0,128,192]
 Unlabelled = [0,0,0]
 
 
